da7cdc4dea109b3bedd89e700be8d72884961b1f
Opty1337 <ricardo.grade@tecnico.ulisboa.pt>
2020-10-14 16:24:34 +0100

    Added ASLRStackLeakExtra CTF

---
+++ Lab_4/BufferOverflows/ASLRStackLeakExtra/Vuln.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void init() {
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+}
+
+void challenge() {
+    char leaky[16];
+    read(0, leaky, 16);
+
+    printf("Leaky buffer: %s\n", leaky);
+    printf("Did you manage to leak anything?\n");
+
+    fgets(leaky, 64, stdin);
+}
+
+int main() {
+    init();
+    challenge();
+    return 0;
+}
